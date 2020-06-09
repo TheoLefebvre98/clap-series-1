058e1884b8763c6549d9174992f84f5a28bd232a
Theo <lefebvre.theo98@gmail.com>
2020-06-09 09:58:14 +0200

    Fixed orderCards.js

--- src/orderCards.js
+++ src/orderCards.js
@@ -9,8 +9,67 @@
  * 
  * On considère que la valeur de la carte prime sur le type de la carte
  */
+
 function orderCards(cards) {
-  // CODE HERE
+  
+  return cards.sort(compare);
+
 }
+
+function compare(card1, card2){
+
+  if(getValue(card1) > getValue(card2))
+    return -1
+  else if(getValue(card1) < getValue(card2))
+    return 1
+  else{
+    if(getType(card1) > getType(card2))
+      return -1;
+    else
+      return 1;
+  }
+
+}
+
+function getValue(card){
+
+  let value = card.slice(0, 1);
+
+  switch(value){
+    case '1' :
+      return 10;
+    case 'J' :
+      return 11;
+    case 'Q' :
+      return 12;
+    case 'K' :
+      return 13;
+    case 'A' :
+      return 14;
+    default : 
+    return Number(value);
+  }
+  
+}
+
+function getType(card){
+
+  let type = card.slice(card.length - 1);
+
+  switch(type){
+    case 'd' :
+      return 1;
+    case 'c' :
+      return 2;
+    case 'h' :
+      return 3;
+    case 's' :
+      return 4;
+  }
+
+}
+
+// J'ai bénéficié de l'aide de Greg pour me mettre sur la voie de cette solution,
+// Puis j'ai écrit le code seul
 
 export { orderCards };
--- testing.js
+++ testing.js
@@ -1,9 +1,62 @@
-function funnyArray(ary) {
-  if(ary.length == 0){
-    return "rien";
-  }else if(ary.length %2 == 0){
-    return ary[(ary.length/2) -1] + ary[ary.length/2];
-  }else{
-    return ary[Math.floor(ary.length/2)];
-  }
+let test = ["Ad", "7h", "8d", "As", "Qs"];
+
+function orderCards(cards) {
+  
+  return cards.sort(compare);
+
 }
+
+function compare(card1, card2){
+
+  if(getValue(card1) > getValue(card2))
+    return -1
+  else if(getValue(card1) < getValue(card2))
+    return 1
+  else{
+    if(getType(card1) > getType(card2))
+      return -1;
+    else
+      return 1;
+  }
+
+}
+
+function getValue(card){
+
+  let value = card.slice(0, 1);
+
+  switch(value){
+    case '1' :
+      return 10;
+    case 'J' :
+      return 11;
+    case 'Q' :
+      return 12;
+    case 'K' :
+      return 13;
+    case 'A' :
+      return 14;
+    default : 
+    return Number(value);
+  }
+  
+}
+
+function getType(card){
+
+  let type = card.slice(card.length - 1);
+
+  switch(type){
+    case 'd' :
+      return 1;
+    case 'c' :
+      return 2;
+    case 'h' :
+      return 3;
+    case 's' :
+      return 4;
+  }
+
+}
+
+console.log(orderCards(test));
